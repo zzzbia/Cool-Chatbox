a649065f4695d1f40e4f780fe081bdeeed42e19c
R a b i a <rabia.saeed.1995@gmail.com>
2022-06-30 15:17:00 -0400

    added emails in seeds because users had hook requiring emails

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -1,75 +1,73 @@
-const router = require('express').Router();
-const { Users, Chat } = require('../../models');
+const router = require("express").Router();
+const { Users, Chat } = require("../../models");
 
 // For getting a user's chat history
-router.get('/:id', async (req, res) => {
-  try {
-    const userData = await Users.findByPk(req.params.id, { include: Chat });
+router.get("/:id", async (req, res) => {
+	try {
+		const userData = await Users.findByPk(req.params.id, { include: Chat });
 
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(400).json(err)
-  }
-})
+		res.status(200).json(userData);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+});
 
 // For user sign up
-router.post('/', async (req, res) => {
-  try {
-    const userData = await Users.create(req.body);
+router.post("/", async (req, res) => {
+	try {
+		const userData = await Users.create(req.body);
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+		req.session.save(() => {
+			req.session.user_id = userData.id;
+			req.session.logged_in = true;
 
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+			res.status(200).json(userData);
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(400).json(err);
+	}
 });
 
 // for login attempts
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await Users.findOne({ where: { username: req.body.username } });
+router.post("/login", async (req, res) => {
+	try {
+		const userData = await Users.findOne({
+			where: { username: req.body.username },
+		});
 
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password' });
-      return;
-    }
+		if (!userData) {
+			res.status(400).json({ message: "Incorrect username or password" });
+			return;
+		}
 
-    const validPassword = await userData.checkPassword(req.body.password);
+		const validPassword = await userData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password' });
-      return;
-    }
+		if (!validPassword) {
+			res.status(400).json({ message: "Incorrect username or password" });
+			return;
+		}
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+		req.session.save(() => {
+			req.session.user_id = userData.id;
+			req.session.logged_in = true;
 
-      res.json({ user: userData, message: `Welcome ${userData.username}` });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
+			res.json({ user: userData, message: `Welcome ${userData.username}` });
+		});
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 //for logging out
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+router.post("/logout", (req, res) => {
+	if (req.session.logged_in) {
+		req.session.destroy(() => {
+			res.status(204).end();
+		});
+	} else {
+		res.status(404).end();
+	}
 });
 
 module.exports = router;
--- models/Users.js
+++ models/Users.js
@@ -1,50 +1,59 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
+	checkPassword(loginPw) {
+		return bcrypt.compareSync(loginPw, this.password);
+	}
 }
 
 User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-  },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.email = await newUserData.email.toLowerCase();
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.email = await updatedUserData.email.toLowerCase();
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'users',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+			},
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [8],
+			},
+		},
+	},
+	{
+		hooks: {
+			beforeCreate: async (newUserData) => {
+				console.log({ newUserData, email: newUserData.users });
+				newUserData.email = await newUserData.email.toLowerCase();
+				return newUserData;
+			},
+			beforeUpdate: async (updatedUserData) => {
+				updatedUserData.email = await updatedUserData.email.toLowerCase();
+				return updatedUserData;
+			},
+		},
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "users",
+	}
 );
 
 module.exports = User;
