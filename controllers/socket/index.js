98c5cfa8efa969b23ea9f1e927848768e016a221
R a b i a <rabia.saeed.1995@gmail.com>
2022-07-03 19:25:54 -0400

    show online users

--- controllers/socket/index.js
+++ controllers/socket/index.js
@@ -1,21 +1,55 @@
 const { Chat, Users, Chat_involvement } = require("../../models");
 
+let users = [];
+
+//[ {
+// 	userName: 'bob',
+// 	userId: 1,
+// 	socketId: 12
+// } ]
+
 const socketController = (io) => {
-	io.use((socket, next) => {
+	io.use(async (socket, next) => {
 		const session = socket.request.session;
 		if (session && session.logged_in) {
 			next();
 		} else {
+			socket.disconnect();
 			next(new Error("unauthorized"));
 		}
 	});
 
 	io.on("connection", (socket) => {
-		console.log("a user connected");
+		socket.on("online", async () => {
+			try {
+				const session = socket.request.session;
+
+				if (!users.some((user) => user.userId === session.user_id)) {
+					const userData = await Users.findByPk(session.user_id);
+					users.push({
+						userName: userData.username,
+						userId: session.user_id,
+						socketId: socket.id,
+					});
+					console.log("users", users);
+					io.emit("users", users);
+				}
+				socket.on("disconnect", () => {
+					users = users.splice(
+						users.findIndex((user) => user.socketId === socket.id),
+						1
+					);
+					io.emit("users", users);
+				});
+			} catch (e) {
+				console.log(e);
+			}
+			console.log("a user connected");
+		});
+
 		const chatId = socket.handshake.query.chatId;
 
 		if (!chatId) {
-			socket.disconnect();
 			return;
 		}
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,60 @@
+const socket = io();
+
+socket.emit("online");
+
+setInterval(() => {
+	socket.emit("online");
+}, 10000);
+
+socket.on("users", (users) => {
+	console.log(users);
+	const usersList = document.getElementById("online-users");
+	usersList.innerHTML = "";
+	for (let i = 0; i < users.length; i++) {
+		// append user to user list
+		const user = document.createElement("li");
+		user.classList.add(
+			"flex",
+			"flex-row",
+			"justify-between",
+			"items-center",
+			"p-2",
+			"bg-indigo-100",
+			"border-b-2",
+			"border-indigo-200",
+			"cursor-pointer"
+		);
+
+		const userId = users[i].userId;
+
+		user.addEventListener("click", () => {
+			fetch("/api/chat/newChat", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					chatPartnerId: userId,
+				}),
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+					if (data.id) {
+						window.location = "/dashboard/chat/" + data.id;
+					}
+				})
+				.catch((e) => {
+					console.log(e);
+				});
+		});
+		user.innerHTML = `<span class="text-sm text-indigo-600">${users[i].userName}</span>`;
+		usersList.appendChild(user);
+	}
+});
+
 const newChatForm = document.getElementById("new-chat");
+
 newChatForm.addEventListener("submit", function (e) {
 	e.preventDefault();
 	const chatPartnerId = document.getElementById("chat-user").value;
